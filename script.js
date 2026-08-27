383904cb5ffb0b939b5b1546e9009c90b1e802ed
sdinesh179 <dineshsammeta179@gmail.com>
2026-08-27 19:04:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,87 @@
+
+
+//1. A variable is a named container for storing data.
+//2. var, let, const
+//3. var variableName = value;
+//4. let variableName = value;
+5//. const variableName = value;
+//6. Declaration creates a variable name. Example: let age;
+//7. Initialization gives a variable its first value. Example: let age = 25;
+//8. Reassignment changes an existing variable’s value. Example: age = 30;
+//9. Redeclaration declares the same variable name again in the same scope.
+//10. var
+//11. var and let
+//12. const
+//13. In let age = 25;:
+  //  - Declaration: let age
+  //
+  //  - Initialization: = 25
+//14. 100
+//15. number = 200;
+//16. 10
+//17. 20
+//18. 30
+//19. var name = "John";
+//20. var price = 500;
+//21. price = 1000;
+//22. 100
+//23. Yes.
+//24. Yes.
+//25.
+//var age = 20;
+//var age = 25;
+//26. let age = 25;
+//27. 30
+//28. Yes.
+//29. No, not in the same scope.
+//30. Error: name has already been declared using let.
+//31. let city = "Chennai";
+//32. city = "Salem";
+//33. 50
+//34. let salary = 25000;
+//35. salary = 30000;
+//36. const pi = 3.14;
+//37. No.
+//38. No.
+//39. const variables must be initialized when declared. Correct code:
+//const age = 25;
+//40. It causes an error because a const value cannot be reassigned.
+//41. const country = "India";
+//42. 100
+//43. const
+//44. let can be reassigned; const cannot be reassigned and must be initialized immediately.
+//45. var can be reassigned and redeclared; const can do neither.
+//46.
+//console.log("Hello World");
+//47. 
+//console.log(500);
+//48. console.warn() displays a warning message in the browser console.
+//49. console.error() displays an error message in the browser console.
+//50.
+ //   - alert() — shows a message box.
+ // - prompt() — asks the user to enter input.  -// confirm() — asks the user to choose OK or Cancel.
+ //  document.writeln() — writes text directly into the webpage.
+  //  - console.log() — prints information in the browser console
+
+
+let Studentname=prompt("Enter your name?")
+let age=prompt("Enter your age?")
+let marks=prompt("Enter your marks?.")
+console.log(Studentname)
+console.log(age)
+console.log(marks)
+
+
+let username=prompt("enter your name?")
+alert(usernmae)
+
+let userage=prompt("Enter your age?")
+console.log(userage)
+
+
+let result=confirm('Do you want to confirm')
+console.log(result)
+
+
+let user=prompt("enter your name?")
+document.writeln(user)
