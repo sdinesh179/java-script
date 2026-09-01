9afff77bfced3953ca757d7666d731e531101f44
sdinesh179 <dineshsammeta179@gmail.com>
2026-09-01 18:33:59 +0530

    Add files via upload

---
+++ Task2.js
@@ -0,0 +1,200 @@
+//Task1 var studentname ="anil";
+    //studentname="Prabha"
+    //console.log(studentname)
+//let studentage=25;
+    //studentage=87;
+    //console.log(studentage)
+//const collegename="avanthi";
+//console.log(studentname)
+//console.log(studentage)
+//console.log(collegename)
+ 
+//var studentname="sunil";
+//console.log(studentname)
+ 
+//let studentage=30;
+//console.log(studentage)
+ 
+//const collegename="sridatta";
+//console.log(collegename)
+ 
+ 
+//Task2 let name="What is your name"
+//let age="what is your age"
+//let city="which city"
+//prompt(name)
+//prompt(age)
+//prompt(city)*/
+ 
+//Task3 let username=prompt("enter your name")
+//alert("Welcome" + username +"!")
+ 
+// Task4 let birth=prompt("enter your year")
+//let year=2026-birth
+//console.log("Birth year:" + birth);
+//console.log("age:" + year);
+ 
+// Task5 let str="Hello"
+//console.log(typeof(str))
+ 
+//let num=100
+//console.log(typeof(num))
+ 
+//var nums=25.5
+//console.log(typeof(nums))
+ 
+//let bol=true
+//console.log(typeof(bol))
+ 
+//var bols=false
+//console.log(typeof(bols))
+ 
+//var b;
+//console.log(b)
+ 
+//var hurr=null;
+//console.log(hurr)
+ 
+ /*Task6:let obj={
+    name:"Anil Reddy",
+    age:25,
+    city:"Hyderabad",
+    qualification:"B-tech",
+    isStudent:"yes"
+}
+ 
+console.log("name:"+obj.name)
+console.log("age:"+obj.age)
+console.log("city:"+obj.city)
+console.log("qualification:"+obj.qualification)
+console.log("isStudent:"+obj.isStudent)*/
+ 
+//Task7 let arr=["Apple","Mango","Orange","Banana","Grapes","Papaya"]
+//console.log(arr[0])
+//console.log(arr[1])
+//console.log(arr[arr.length-1])
+//console.log((arr.length))
+ 
+// Task8 let a=20;
+//let b=5;
+//console.log(a+b)
+ 
+//let a=20;
+//let b=5;
+//console.log(a-b)
+ 
+//let a=20;
+//let b=5;
+//console.log(a*b)
+ 
+//let a=20;
+//let b=5;
+//console.log(a/b)
+ 
+//let a=20;
+//let b=5;
+//console.log(a%b)
+ 
+//let a=20;
+//let b=5;
+//console.log(a%b)
+ 
+//let a=20;
+//let b=5;
+//console.log(a**b)
+ 
+//Task9 shirt=999
+//pant=1499
+//shoes=1999
+//total=shirt+pant+shoes
+//console.log("Total:"+total)
+ 
+// Task10 Tamil=80
+//english=75
+//maths=90
+//total=Tamil+english+maths
+//console.log("Total:"+total)
+//console.log("Average:"+total/3)
+ 
+// Task11 let a=10;
+//let b=a++;
+//console.log(a)
+//console.log(b)
+ 
+//Task12 let a=10;
+//let b=++a;
+//console.log(a)
+//console.log(b)
+ 
+//Task13 let a =20;
+//let b=a--;
+//console.log(a)
+//console.log(b)
+ 
+//Task14 let a=20;
+//let b=--a;
+//console.log(a)
+//console.log(b)
+ 
+//Task15 let a=5;
+//let b=a++;
+//let c=++a;
+//let d=b--;;
+//console.log(a)
+//console.log(b)
+//console.log(c)
+//console.log(d)
+ 
+//Task16 let num=10;
+//num +=10;
+//console.log(num)
+ 
+//let num=10;
+//num -=10;
+//console.log(num)
+ 
+//var num=10;
+//num *=10;
+//console.log(num)
+ 
+//var num=10;
+//num /=10;
+//console.log(num)
+ 
+//var num=10;
+//num %=10;
+//console.log(num)
+ 
+//var num=10;
+//num **=10;
+//console.log(num)
+ 
+ 
+ 
+ 
+ 
+//Task 17 let student={
+    //name:"Mahendar",
+    //age:27,
+    //city:"Nalgonda",
+    //subjects:["Maths","science","scocisl","hindi","urdu"],
+    //isStudent:"yes"
+//}
+ 
+//console.log(student.name)
+//console.log(student.age)
+//console.log(student.city)
+//console.log(student.subjects[0])
+//console.log(student.subjects.length-1)
+//console.log(student.subjects.length)
+//console.log(student)
+ 
+let num1 = Number(prompt("Enter first number"));
+let num2 = Number(prompt("Enter second number"));
+ 
+console.log("Addition:", num1 + num2);
+console.log("Subtraction:", num1 - num2);
+console.log("Multiplication:", num1 * num2);
+console.log("Division:", num1 / num2);
+console.log("Division:", num1 % num2);
+console.log("Division:", num1 ** num2);
